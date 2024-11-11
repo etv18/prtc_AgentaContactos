850d512f7c1a8ddf18ab54e0bd44c81d799c32c7
Emmanuel Tavarez <tavareze2003@gmail.com>
2024-11-10 22:07:38 -0400

    primer boceto de icono de usuario en edit y show de las vistas contactos

--- resources/views/contactos/edit.blade.php
+++ resources/views/contactos/edit.blade.php
@@ -1,10 +1,21 @@
 <x-contact-layout>
     <div class="w-[70%] mx-auto sm:px-6 lg:px-8">
-        <form class="max-w-md ml-0 w-full" action="{{ route('contactos.update', $contacto->id) }}" method="POST">
+        <form class="max-w-md ml-0 w-full" action="{{ route('contactos.update', $contacto->id) }}" method="POST" style="margin-top: -5px;">
             @csrf @method('PUT')
             <div class="py-12 w-full">
-                <div class="px-10 py-14 bg-green-800 rounded-md relative z-0 w-full mb-5 group">pedro</div>
-                <div class="grid md:grid-cols-2 md:gap-6">
+                <center>
+                    <div class="px-1.5 py-16 bg-blue-950 rounded-full relative w-1/2 z-0 mb-5 group flex flex-col items-center justify-center">
+                        <!-- Ícono de usuario -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="text-white mb-2" style="width: 25%; height:25%;" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-3.87 0-7 1.28-7 4v2h14v-2c0-2.72-3.13-4-7-4z" />
+                        </svg>
+                        <span class="text-white font-medium text-xl">{{$contacto->nombre}}</span>
+                    </div>
+                </center>
+                <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Guardar Edicion</button>
+                <a href="{{ route('contactos.show', $contacto->id) }}" class="ml-2 text-white bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">Descartar</a>
+
+                <div class="mt-8 grid md:grid-cols-2 md:gap-6">
                     <div class="relative z-0 w-full mb-5 group">
                         <input type="text" name="nombre" id="nombre" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value="{{ $contacto->nombre }}" />
                         <label for="nombre" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nombre</label>
@@ -47,7 +58,6 @@
                     <label for="nota" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Notas</label>
                     <input type="text" id="nota" name="nota" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{ $contacto->nota }}">
                 </div>
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Guardar Edicion</button>
             </div>
         </form>
     </div>
--- resources/views/contactos/show.blade.php
+++ resources/views/contactos/show.blade.php
@@ -1,10 +1,21 @@
 <x-contact-layout>
     <div class="w-[70%] mx-auto sm:px-6 lg:px-8">
         <div class="max-w-md ml-0 w-full">
-            @csrf
             <div class="py-12 w-full">
-                <div class="px-10 py-14 bg-green-800 rounded-md relative z-0 w-full mb-5 group">pedro</div>
-                <div class="grid md:grid-cols-2 md:gap-6">
+                <center>
+                    <div class="px-1.5 py-16 bg-blue-950 rounded-full relative w-1/2 z-0 mb-5 group flex flex-col items-center justify-center">
+                        <!-- Ícono de usuario -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="text-white mb-2" style="width: 25%; height:25%;" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-3.87 0-7 1.28-7 4v2h14v-2c0-2.72-3.13-4-7-4z" />
+                        </svg>
+                        <span class="text-white font-medium text-xl">{{$contacto->nombre}}</span>
+                    </div>
+                </center>
+                <div class="btns">
+                    <a href="{{ route('contactos.index') }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Volver a Inicio</a>
+                    <a href="{{ route('contactos.edit', $contacto->id) }}" class="ml-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Editar</a>
+                </div>
+                <div class="mt-8 grid md:grid-cols-2 md:gap-6">
                     <div class="relative z-0 w-full mb-5 group">
                         <input type="text" name="nombre" id="nombre" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value="{{ $contacto->nombre }}" disabled />
                         <label for="nombre" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nombre</label>
@@ -47,7 +58,6 @@
                     <label for="nota" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Notas</label>
                     <input type="text" id="nota" name="nota" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{ $contacto->nota }}" disabled>
                 </div>
-                <a href="{{ route('contactos.edit', $contacto->id) }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Editar</a>
             </div>
         </div>
     </div>
